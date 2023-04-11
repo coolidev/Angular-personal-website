3ebec357d50b62a5912b1da624d9aade245d644a
coolidev <coolidev@hotmail.com>
2023-04-11 15:30:50 -0400

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+
+  SANITY_PROJECT_ID:"hf9j63to",
+  SANITY_DATASET:"production",
+  SANITY_API_VERSION:"2022-11-15",
+  SANITY_PROJECT_TITLE:"BackPortfolio",
 };
